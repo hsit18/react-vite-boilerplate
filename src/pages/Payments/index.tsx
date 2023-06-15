677bef466123fe37cc1a19077330f55eda542897
Harpreet Singh <hsit18@gmail.com>
2023-06-15 14:23:21 +0000

    loading component in progress

--- src/pages/Payments/index.tsx
+++ src/pages/Payments/index.tsx
@@ -4,12 +4,15 @@ import {
     useQuery,
 } from '@tanstack/react-query';
 import { getPaymentData } from "@/services/paymentData";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Payments = () => {
     const query = useQuery({ queryKey: ['payments'], queryFn: getPaymentData });
 
     return (
         <div className="container mx-auto py-10">
+            {query.isLoading && <Skeleton className="w-[100px] h-[20px] rounded-full" />}
+
             <DataTable columns={columns} data={query.data || []} />
         </div>
     )
